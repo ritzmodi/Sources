c647a01f04c9c199b8b2c82ddde5763c6063e0e9
Ritesh Modi <desiredstate@outlook.com>
2018-04-25 02:11:20 -0400

    iot

---
+++ DeviceIdentity/DeviceIdentity/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+
+namespace DeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=mclass.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=unxYBC2AeqKjWYW5OwtY1jFeSAp/4S339yVZOQX5n5A=";
+       
+        
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+
+        }
+
+        private static async Task AddDeviceAsync() {
+            string deviceID = "mydeviceid1";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceID));
+            }
+            catch (DeviceAlreadyExistsException ex)
+            {
+                device = await registryManager.GetDeviceAsync(deviceID);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+
+        }
+    }
+}
---
+++ DeviceIdentity/Sender/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+
+namespace Sender
+{
+    class Program
+    {
+
+        static DeviceClient deviceClient;
+        static string connectionString;
+        static string deviceID;
+
+
+        static void Main(string[] args)
+        {
+            deviceClient = DeviceClient.Create("mclass.azure-devices.net", new DeviceAuthenticationWithRegistrySymmetricKey("mydeviceid1", "LVJzz3T8kJQ8ZKbmIYBMMUYvkBDtEYdwMFCmiiJ8n5M="));
+            deviceClient.ProductInfo = "humidityapp";
+            SendMessages();
+
+            Console.ReadLine();
+        }
+
+
+        private static async void SendMessages()
+        {
+            int mid = 1;
+            Random rnd = new Random();           
+
+            while (true) {
+                
+                double output = rnd.NextDouble() * 10;
+                double h = output * 3;
+
+                var tempdata = new {
+                    deviceName = "mydeviceid1",
+                    tempdata = output,
+                    humidity = h,
+                    messageID = mid++
+
+                };
+                var messageStr = JsonConvert.SerializeObject(tempdata);
+
+                var message = new Message(Encoding.ASCII.GetBytes(messageStr));
+
+                message.Properties.Add("humidityAlert", (h > 20) ? "true" : "false");
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine(messageStr);
+
+                await Task.Delay(1000);
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
