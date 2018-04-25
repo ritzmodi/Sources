52b33f6ea2c22a57bea22ef0e6cf1c3d82ec1cab
Ritesh Modi <desiredstate@outlook.com>
2018-04-25 05:19:45 -0400

    cognitive

---
+++ Cognitive/sentiment.ps1
@@ -0,0 +1,24 @@
+﻿
+
+$EmotionAPIURI =   "https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment"
+
+$apiKey = "059a41b7360441c983a1a9aaf0766eb9"
+
+ $query = "?Subscription-Key=$apiKey"
+ $uri = $EmotionAPIURI+$query  
+
+$documents = @()
+
+$message = @{"language" = "en"; "id" = "1"; "text" = "I had a wonderful experience! The rooms were wonderful and the staff were helpful." };
+
+$documents += $message
+
+$final = @{documents = $documents}
+
+$messagePayload = ConvertTo-Json $final
+ 
+ $result = Invoke-RestMethod -Method Post -Uri $uri -Header @{ "Ocp-Apim-Subscription-Key" = $apiKey } -Body $messagePayload -ContentType application/json -ErrorAction Stop            
+
+# $result.regions.lines.words.text
+
+
---
+++ Cognitive/tags.ps1
@@ -0,0 +1,16 @@
+﻿
+Add-Type -AssemblyName "System.Net.Http" 
+$EmotionAPIURI = "https://westeurope.api.cognitive.microsoft.com/vision/v1.0/tag"  
+
+
+$apiKey = "9d9ad91e5ce7439386523a45d8fb2242"
+
+ $query = "?Subscription-Key=$apiKey"
+ $uri = $EmotionAPIURI+$query  
+
+ 
+ $result = Invoke-RestMethod -Method Post -Uri $uri -Header @{ "Ocp-Apim-Subscription-Key" = $apiKey } -InFile "C:\Users\rites\Desktop\MCT\ocr3.jpg" -ContentType "application/octet-stream" -ErrorAction Stop            
+
+# $result.regions.lines.words.text
+
+
